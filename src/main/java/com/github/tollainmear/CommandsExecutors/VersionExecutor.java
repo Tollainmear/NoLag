ec5d7d2ea7e82bc02137b056cd8308f377fd3b9a
Mori <mori.fuyun@gmail.com>
2018-02-24 09:52:07 +0800

    Builder Version return

--- src/main/java/com/github/tollainmear/CommandsExecutors/VersionExecutor.java
+++ src/main/java/com/github/tollainmear/CommandsExecutors/VersionExecutor.java
@@ -1,15 +1,22 @@
 package com.github.tollainmear.CommandsExecutors;
 
+import com.github.tollainmear.NoLag;
+import com.github.tollainmear.Translator;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.Text;
 
 public class VersionExecutor implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         //todo-向指令源发送版本信息
-        return null;
+        Translator translator = NoLag.getInstance().getTranslator();
+        Text i = translator.getText("version");
+        src.sendMessage(i);
+        return CommandResult.success();
     }
 }
